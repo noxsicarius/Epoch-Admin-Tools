268e61d4e2f40ecf3ebe72eb5c5ff98fe8d39489
Sandbird <sandbird@gmail.com>
2014-02-21 21:30:06 +0200

    - Improved menu with Categories

--- admintools/dialog.hpp
+++ admintools/dialog.hpp
@@ -147,40 +147,79 @@ class AdminDialog
 			h = 0.55016 * safezoneH;
 			colorBackground[] = {0,0,0,0.7};
 		};
+		class RscText_1001: RscTextT
+		{
+			idc = 1001;
+			text = "Pick a vehicle or select a category";
+			x = 0.295498 * safezoneW + safezoneX;
+			y = 0.235917 * safezoneH + safezoneY;
+			w = 0.170212 * safezoneW;
+			h = 0.0166935 * safezoneH;
+		};
 		class RscListbox_1500: RscListbox
 		{
 			idc = 13000;
 			x = 0.290407 * safezoneW + safezoneX;
-			y = 0.238674 * safezoneH + safezoneY;
+			y = 0.268674 * safezoneH + safezoneY;
 			w = 0.419186 * safezoneW;
-			h = 0.467636 * safezoneH;
+			h = 0.447636 * safezoneH;
 		};
 		class RscShortcutButton_1700: RscShortcutButton
 		{
 			idc = -1;
-			text = "Perm";
-			x = 0.297168 * safezoneW + safezoneX;
-			y = 0.692556 * safezoneH + safezoneY;
-			w = 0.0651391 * safezoneW;
-			h = 0.0590864 * safezoneH;
-			onButtonClick = "PermDialogSelected = (lbCurSel 13000); ((ctrlParent (_this select 0)) closeDisplay 9000);";
-		};
-		class RscShortcutButton_1701: RscShortcutButton
-		{
-			idc = -1;
 			text = "Temp";
-			x = 0.37154 * safezoneW + safezoneX;
-			y = 0.692556 * safezoneH + safezoneY;
+			x = 0.297168 * safezoneW + safezoneX;
+			y = 0.720556 * safezoneH + safezoneY;
 			w = 0.0651391 * safezoneW;
 			h = 0.0590864 * safezoneH;
 			onButtonClick = "TempDialogSelected = (lbCurSel 13000); ((ctrlParent (_this select 0)) closeDisplay 9000);";
 		};
+		class RscShortcutButton_1701: RscShortcutButton
+		{
+			idc = -1;
+			text = "Perm";
+			x = 0.37154 * safezoneW + safezoneX;
+			y = 0.720556 * safezoneH + safezoneY;
+			w = 0.0651391 * safezoneW;
+			h = 0.0590864 * safezoneH;
+			onButtonClick = "PermDialogSelected = (lbCurSel 13000); ((ctrlParent (_this select 0)) closeDisplay 9000);";
+		};
 		class RscShortcutButton_1702: RscShortcutButton
+		{
+			idc = 12004;
+			text = "Air";
+			x = 0.485168 * safezoneW + safezoneX;
+			y = 0.218000 * safezoneH + safezoneY;
+			w = 0.0651391 * safezoneW;
+			h = 0.0590864 * safezoneH;
+			onButtonClick="[""Air""] call LoadSpecificList;";
+		};
+		class RscShortcutButton_1704: RscShortcutButton
+		{
+			idc = 12005;
+			text = "Land";
+			x = 0.55954 * safezoneW + safezoneX;
+			y = 0.218000 * safezoneH + safezoneY;
+			w = 0.0651391 * safezoneW;
+			h = 0.0590864 * safezoneH;
+			onButtonClick="[""LandVehicle""] call LoadSpecificList;";
+		};
+		class RscShortcutButton_1705: RscShortcutButton
+		{
+			idc = 12006;
+			text = "Sea";
+			x = 0.633912 * safezoneW + safezoneX;
+			y = 0.218000 * safezoneH + safezoneY;
+			w = 0.0651391 * safezoneW;
+			h = 0.0590864 * safezoneH;
+			onButtonClick="[""Ship""] call LoadSpecificList;";
+		};
+		class RscShortcutButton_1710: RscShortcutButton
 		{
 			idc = -1;
 			text = "Close";
-			x = 0.445912 * safezoneW + safezoneX;
-			y = 0.692556 * safezoneH + safezoneY;
+			x = 0.633912 * safezoneW + safezoneX;
+			y = 0.720556 * safezoneH + safezoneY;
 			w = 0.0651391 * safezoneW;
 			h = 0.0590864 * safezoneH;
 			onButtonClick = "((ctrlParent (_this select 0)) closeDisplay 9000);";
--- admintools/tools/addvehicleDialog.sqf
+++ admintools/tools/addvehicleDialog.sqf
@@ -36,6 +36,40 @@ lbClear AdminDialogList;
   lbSetPicture [AdminDialogList, _index, _image];
 	
 } forEach vhnlist;
+
+LoadSpecificList = {
+	private ["_kindOf", "_filter", "_cfgvehicles","_dialog","_vehicle"];
+	lbClear AdminDialogList;
+		vhnlist = [];
+		//_kindOf = ["LandVehicle","Air","Ship"];
+		_kindOf = [_this select 0];
+		_filter = ["BIS_Steerable_Parachute","ParachuteBase","StaticWeapon"];      
+		_cfgvehicles = configFile >> "cfgVehicles";
+		titleText ["Generating Vehicle list... Please wait ...","PLAIN DOWN"];titleFadeOut 2;
+		for "_i" from 0 to (count _cfgvehicles)-1 do 
+		{
+			private ["_vehicle", "_veh_type", "_textPart"];
+			_vehicle = _cfgvehicles select _i;
+			if (isClass _vehicle) then {
+				_veh_type = configName(_vehicle);
+				if ((getNumber(_vehicle >> "scope") == 2) and (getText(_vehicle >> "picture") != "") and (KINDOF_ARRAY(_veh_type,_kindOf)) and !(KINDOF_ARRAY(_veh_type,_filter))) then {
+					_textPart =	getText(configFile >> "cfgVehicles" >> _veh_type >> "displayName");
+					_image = (gettext (configFile >> "CfgVehicles" >> _veh_type >> "picture"));
+					vhnlist set [count vhnlist,[_veh_type, _textPart, _image]];
+				};
+			};
+		};
+		titleText ["List is ready...","PLAIN DOWN"];titleFadeOut 2;
+	
+	lbClear AdminDialogList;
+	{
+		private ["_index", "_x","_image"];
+		_image = _x select 2;
+		_index = lbAdd [AdminDialogList, format["%2 (%1)", _x select 0, _x select 1]];
+	  lbSetPicture [AdminDialogList, _index, _image];
+	} forEach vhnlist;
+};
+
 waitUntil { !dialog };
 if ((PermDialogSelected < 0) && (TempDialogSelected < 0)) exitWith {};
 
