eb1fba0047f6c61e791c20625e06f6769197e076
Sandbird <sandbird@gmail.com>
2014-02-21 17:55:58 +0200

    -Fix for dialog.hpp
    Now it wont crash anymore...forgot to add the button definitions.

--- admintools/dialog.hpp
+++ admintools/dialog.hpp
@@ -4,6 +4,134 @@
 
 // Control types
 
+class RscTextT
+{
+	access = 0;
+	type = 0;
+	idc = -1;
+	colorBackground[] = {0,0,0,0};
+	colorText[] = {0.8784,0.8471,0.651,1};
+	text = "";
+	fixedWidth = 0;
+	x = 0;
+	y = 0;
+	h = 0.037;
+	w = 0.3;
+	style = 0;
+	shadow = 2;
+	font = "Zeppelin32";
+	SizeEx = 0.03921;
+};
+class RscListBox
+{
+	access = 0;
+	type = 5;
+	w = 0.4;
+	h = 0.4;
+	rowHeight = 0;
+	colorText[] = {0.8784,0.8471,0.651,1};
+	colorScrollbar[] = {0.95,0.95,0.95,1};
+	colorSelect[] = {0.95,0.95,0.95,1};
+	colorSelect2[] = {0.95,0.95,0.95,1};
+	colorSelectBackground[] = {0,0,0,1};
+	colorSelectBackground2[] = {0.8784,0.8471,0.651,1};
+	colorBackground[] = {0,0,0,1};
+	soundSelect[] = {"",0.1,1};
+	arrowEmpty = "#(argb,8,8,3)color(1,1,1,1)";
+	arrowFull = "#(argb,8,8,3)color(1,1,1,1)";
+	class ScrollBar
+	{
+		color[] = {1,1,1,0.6};
+		colorActive[] = {1,1,1,1};
+		colorDisabled[] = {1,1,1,0.3};
+		shadow = 0;
+		thumb = "\ca\ui\data\ui_scrollbar_thumb_ca.paa";
+		arrowFull = "\ca\ui\data\ui_arrow_top_active_ca.paa";
+		arrowEmpty = "\ca\ui\data\ui_arrow_top_ca.paa";
+		border = "\ca\ui\data\ui_border_scroll_ca.paa";
+	};
+	style = 16;
+	font = "Zeppelin32";
+	shadow = 2;
+	sizeEx = 0.03921;
+	color[] = {1,1,1,1};
+	period = 1.2;
+	maxHistoryDelay = 1;
+	autoScrollSpeed = -1;
+	autoScrollDelay = 5;
+	autoScrollRewind = 0;
+};
+class RscShortcutButton
+{
+	type = 16;
+	x = 0.1;
+	y = 0.1;
+	class HitZone
+	{
+		left = 0.004;
+		top = 0.029;
+		right = 0.004;
+		bottom = 0.029;
+	};
+	class ShortcutPos
+	{
+		left = 0.0145;
+		top = 0.026;
+		w = 0.0392157;
+		h = 0.0522876;
+	};
+	class TextPos
+	{
+		left = 0.05;
+		top = 0.034;
+		right = 0.005;
+		bottom = 0.005;
+	};
+	shortcuts[] = {};
+	textureNoShortcut = "#(argb,8,8,3)color(0,0,0,0)";
+	color[] = {0.8784,0.8471,0.651,1};
+	color2[] = {0.95,0.95,0.95,1};
+	colorDisabled[] = {1,1,1,0.25};
+	colorBackground[] = {1,1,1,1};
+	colorBackground2[] = {1,1,1,0.4};
+	class Attributes
+	{
+		font = "Zeppelin32";
+		color = "#E5E5E5";
+		align = "left";
+		shadow = "true";
+	};
+	idc = -1;
+	style = 0;
+	default = 0;
+	shadow = 2;
+	w = 0.183825;
+	h = 0.104575;
+	periodFocus = 1.2;
+	periodOver = 0.8;
+	animTextureNormal = "\ca\ui\data\ui_button_normal_ca.paa";
+	animTextureDisabled = "\ca\ui\data\ui_button_disabled_ca.paa";
+	animTextureOver = "\ca\ui\data\ui_button_over_ca.paa";
+	animTextureFocused = "\ca\ui\data\ui_button_focus_ca.paa";
+	animTexturePressed = "\ca\ui\data\ui_button_down_ca.paa";
+	animTextureDefault = "\ca\ui\data\ui_button_default_ca.paa";
+	period = 0.4;
+	font = "Zeppelin32";
+	size = 0.03921;
+	sizeEx = 0.03921;
+	text = "";
+	soundEnter[] = {"\ca\ui\data\sound\onover",0.09,1};
+	soundPush[] = {"\ca\ui\data\sound\new1",0,0};
+	soundClick[] = {"\ca\ui\data\sound\onclick",0.07,1};
+	soundEscape[] = {"\ca\ui\data\sound\onescape",0.09,1};
+	action = "";
+	class AttributesImage
+	{
+		font = "Zeppelin32";
+		color = "#E5E5E5";
+		align = "left";
+	};
+};
 class AdminDialog
 {
 	idd = -1;
